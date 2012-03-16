87b997455cdf3db974fca7afde92ee35befd7bee
cytec <iamcytec@googlemail.com>
2012-03-16 15:46:39 +0100

    added twitter DM controls

--- PyBurn.py
+++ PyBurn.py
@@ -25,19 +25,65 @@ from os.path import join
 from os.path import isfile
 from shutil import move
 import time
+import twitter
+import re
 
 from module.plugins.Hook import Hook
 
-class PyBurn(Hook):
-    __name__ = "PyBurn"
+class pyBurn(Hook):
+    __name__ = "pyBurn"
     __version__ = "0.1"
     __description__ = """Control pyLoad via Twitter and other cool stuff"""
     __config__ = [ ("activated", "bool", "Activated" , "False"),
-                   ("folder", "str", "Folder to observe", "container"),
-                   ("watch_file", "bool", "Observe link file", "False"),
-                   ("keep", "bool", "Keep added containers", "True"),
-                   ("file", "str", "Link file", "links.txt")]
+                    ("twinterval", "int", "Seconds between request to Twitter", "30"),
+                    ("UserWhitelist", "str", "Only allow Specific user to interact via DM", "TwitterUser"),
+                    ("whiteUserOnly", "bool", "Only allow users listed above to interact via Twitter DM", "False"),
+                    ("twitresponse", "bool", "Send response Message when Commands where executed?", "False"),
+                    ("twitify", "bool", "Notifications over twitter when a Package Download finished?", "False"),
+                    ("twitifyDM", "bool", "Notifications via DM?", "False"),
+                    ("twitifyUser", "str", "Name of the User that should be notified", "TwitterUser")]
     __threaded__ = []
     __author_name__ = ("cytec")
     __author_mail__ = ("iamcytec@googlemail.com")
     
+    def setup(self):
+        self.interval = self.getConfig('twinterval')
+
+    def periodical(self):
+        api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='', access_token_secret='')
+        self.logDebug("Getting DirectMessages from Twitter")
+        messages = api.GetDirectMessages()
+        for dm in messages:
+            fromUser = dm.sender_screen_name
+            if self.getConfig('whiteUserOnly'):
+                self.logDebug("Useing Whitelist Filter for DMs")
+                if fromUser in self.getConfig('UserWhitelist'):
+                    dmcontent = dm.text
+            else:
+                dmcontent = dm.text
+            self.logDebug("Message from: %s with text: %s" % (fromUser, dmcontent))
+            splitDM = dmcontent.split()
+            if splitDM[0].lower() == "addlink" or splitDM[0].lower() == "addlinks":
+                splitDM.remove(splitDM[0])
+                if splitDM[0].startswith('http'):
+                    pkgname = "Twitter_%s_%s" % (fromUser, time.strftime("%H-%M-%S_%d%b%Y"))
+                    self.core.api.addPackage(pkgname, splitDM, 1)
+                else:
+                    pkgname = splitDM[0]
+                    splitDM.remove(splitDM[0])
+                    self.core.api.addPackage(pkgname, splitDM, 1)
+                #for link in splitDM:
+                self.logInfo("Added: %s Links to Package: %s" % (len(splitDM), pkgname))
+                api.DestroyDirectMessage(dm.id)
+                self.logDebug("Deleted: DM with id %s from User: %s" % (dm.id, fromUser))
+                if self.getConfig('twitresponse'):
+                    twitifyUser = self.getConfig('twitifyUser')
+                    if self.getConfig('twitifyDM'):
+                        api.PostDirectMessage(twitifyUser, "Added: %s Links to Package: %s" % (len(splitDM), pkgname))
+                        self.logDebug("DM send to %s" % (twitifyUser))
+                    else:
+                        api.PostUpdate("@%s Added: %s Links to Package: %s" % (twitifyUser, len(splitDM), pkgname))
+                        self.logDebug("Note send to @%s" % (twitifyUser))
+
+
+
--- README.md
+++ README.md
@@ -1,10 +1,77 @@
 # pyBurn
 
-Hook for pyLoad.
+ive rewritten my bash scripts to python... yeah!
+so now pyBurn is a hook for pyLoad itself.
 
-* unrar multirar archives
-* renaming for series
-* twitter and boxcar notifications 
-* twitter DM controls (status/addlink/dlc)
+## NOTE!
+Currently this is in heavy development.
+So if you like to test, feel free to do so but notice that this is still a verry early version 
 
-much more to come
+## Current features:
+
+***Add new Links over Twitter***
+Usage: AddLinks {CONTAINERNAME} LINK LINK LINK
+If no Containername it generates one based on DM Sendername and Date.
+
+***Twitter Response***
+Activate the Twitter Response Options in the Settings and pyBurn will send you either an DM or an @ reply after adding the links from your AddLinks command.
+
+
+##Requiers:
+
+* pyLoad
+* python-twitter
+* twitter API keys (http://twitter.com/apps)
+
+## How To:
+
+Install pyLoad.
+
+Install python-twitter
+
+go to http://twitter.com/apps and create an app, be sure you select DM access under rights.
+
+***Download pyBurn**** 
+open pyBurn in your Favorite text editor and change this line:
+<code>api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='', access_token_secret='')</code>
+to contain your app keys
+(copy/move it to module/plugins/hooks)
+
+Start pyLoad
+
+Activate/Change Settings in the Webinterface.
+
+## The Settings:
+
+***Activated:***
+Plugin Activated (no/yes) 
+default: no
+
+***Interval: ***
+Interval to run in seconds
+default: 30
+
+***Twitter Response:***
+Sends back a Response after a command was executed to nitify you (no/yes)
+default no
+
+***Use DM Whitelist***
+Use Whitelist of Twitter users (no/yes)
+YOU NEED TO SET THE USERNAME IN "DM Whitelist" (seperated by blanks)
+default no
+
+***DM Whitelist: ***
+only accept DM's from this User (ONLY NEEDED IF "Use DM Whitelist" is TRUE)
+default none
+
+***Twitter Notify:***
+Send Notifications when Package Downloads are finished (no/yes)
+default no
+
+***Use DM for Notify:***
+Use DM instead of @ reply for ANY KIND notifications/replys (no/yes)
+default no
+
+***Twitter Notify User:***
+Use this if you like to Notify another like the one who send the connand, leve it as it is to send notes back to the sender
+default none
